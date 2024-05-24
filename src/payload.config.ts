eea4e6e4ea35fe8bc5809414315bea22899b36e5
Aditya Khare <adikhare24@gmail.com>
2024-05-24 21:46:52 +0530

    add : updated docker files and switch to cloud storage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,8 @@
 import { buildConfig } from "payload/config";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { cloudStorage } from "@payloadcms/plugin-cloud-storage";
+import { s3Adapter } from "@payloadcms/plugin-cloud-storage/s3";
 import seo from "@payloadcms/plugin-seo";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
@@ -15,6 +17,18 @@ import { Reviews } from "./collections/Reviews";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
+});
+
+const adapter = s3Adapter({
+  config: {
+    endpoint: process.env.S3_ENDPOINT!,
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+    },
+    region: process.env.S3_REGION!,
+  },
+  bucket: process.env.S3_BUCKET!,
 });
 
 export default buildConfig({
@@ -59,6 +73,16 @@ export default buildConfig({
         return "Shopaggator - the marketplace for digital assets";
       },
       uploadsCollection: "media",
+    }),
+    cloudStorage({
+      collections: {
+        media: {
+          adapter,
+        },
+        product_files: {
+          adapter,
+        },
+      },
     }),
   ],
 });
